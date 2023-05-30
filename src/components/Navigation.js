c7bda9d2b321e3365df0788a7c39840c553c89e0
Xuan Quy <hoangxuanquy5@gmail.com>
2023-05-30 15:49:56 +0700

    use State Hook

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -22,9 +22,9 @@ export default function Navigation() {
     <nav>
   <ul>
       <li><a className='active' href='#home'>Home</a></li>
-      <li><a href='#news'>News</a></li>
-      <li><a href='#about'>About</a></li>
-      <li><a href='#contact'>Contact</a></li>
+      <li><a className='active' href='#news'>News</a></li>
+      <li><a className='active' href='#about'>About</a></li>
+      <li><a className='active' href='#contact'>Contact</a></li>
   </ul>
 </nav>
   )
--- src/components/Players.js
+++ src/components/Players.js
@@ -93,13 +93,14 @@ export default function Players() {
             <img src={player.img} alt={player.name} />
             <h3>{player.name}</h3>
             <p className='title'>{player.club}</p>
-            <p className='btn'><button onClick={()=>{setPlayer(player)}}><a href='#popup1' id='openPopUp'>Detail</a></button></p>
+            <p className='btn'><button onClick={() => { setPlayer(player) }}>
+              <a href='#popup1' id='openPopUp'>Detail</a></button></p>
           </div>
         </div>
       ))}
       <div id='popup1' className='overlay'>
         <div className='popup'>
-          <img src={player.img}/>
+          <img src={player.img} />
           <h2>{player.name}</h2>
           <a className='close' href='#'>&times;</a>
           <div className='content'>
@@ -110,4 +111,3 @@ export default function Players() {
     </div>
   );
 }
-      
